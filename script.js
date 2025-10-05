81c5d59b5843496991e74be1946bd3995533f7e2
Khuseyn <khuseyn61art@gmail.com>
2025-10-05 22:01:17 +0500

    крч много чего сделал уже не помню

--- script.js
+++ script.js
@@ -6,6 +6,36 @@ const randomBtn = document.getElementById("random-btn");
 const errorMsg = document.getElementById("error-msg");
 
 
-let choice = [];
+let choices = [];
 
-form.addEventListener("submit")
+const savedChoices = localStorage.getItem("choices");
+if (savedChoices) {
+    choices = JSON.parse(savedChoices);
+}
+
+
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const value = input.value;
+    console.log("Ведено", value);
+
+    if (value.trim() === "") {
+        errorMsg.classList.remove("hidden");
+    } else {
+        errorMsg.classList.add("hidden");
+        choices.push(value.trim());
+        input.value = "";
+    }
+});
+
+randomBtn.addEventListener("click", function () {
+    if (choices.length == 0) {
+        result.textContent = "Сначала добавьте варианты!";
+    } else {
+        const rand = Math.floor(Math.random() * choices.length);
+        const selected = choices[rand];
+        result.textContent = " Выбор: " + selected
+    }
+});
