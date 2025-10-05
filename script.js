e752832042789e87c00585d94d80a8b7233395c5
Khuseyn <khuseyn61art@gmail.com>
2025-10-05 21:40:33 +0500

    Убрал коснол логи

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 const form = document.getElementById("choice-form");
-console.log(form);
 const input = document.getElementById("choice-input");
-console.log(input);
 const list = document.getElementById("choice-list");
-console.log(list);
 const result = document.getElementById("result");
-console.log(result);
 const randomBtn = document.getElementById("random-btn");
-console.log(randomBtn);
 const errorMsg = document.getElementById("error-msg");
-console.log(errorMsg);
 
 
 let choice = [];
